68e537fb860af6c50a570d2e41fa95a6a34402fe
JavachipMom <Nicolesp4ever@gmail.com>
2017-02-03 16:38:40 -0600

    created static results page

--- js/index.js
+++ js/index.js
@@ -4,6 +4,11 @@ import { Router, Route, hashHistory } from 'react-router'
 import App from './app'
 import Main from './main'
 import Results from './results'
+import Results2 from './results2'
+import Results3 from './results3'
+import Results4 from './results4'
+import Results5 from './results5'
+import Results6 from './results6'
 
 render(
   (
@@ -11,6 +16,11 @@ render(
      <Route component={App}>
        <Route path="/" component={Main} />
        <Route path="results" component={Results} />
+       <Route path="results2" component={Results2} />
+       <Route path="results3" component={Results3} />
+       <Route path="results4" component={Results4} />
+       <Route path="results5" component={Results5} />
+       <Route path="results6" component={Results6} />
      </Route>
    </Router>
   ),
