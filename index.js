ada5aff20e844681c4089e6e3aca75bc605a1eb1
JavachipMom <Nicolesp4ever@gmail.com>
2017-02-07 09:49:16 -0600

    Brought in the yelp API

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+import API from 'API-keys'
+
 var express = require('express');
 var app = express();
+
+var yelp = require('yelp-fusion');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -11,6 +15,38 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
+});
+
+// Make these enviroment variables to keep my clientId and API key hidden without losing the data pulled in from Yelp
+
+var yelpClient;
+
+yelp.accessToken(appId, appSecret).then(response => {
+  yelpClient = yelp.client(response.jsonBody.access_token);
+}).catch(e => {
+  console.log(e);
+});
+
+app.get('/yelpdata.json', function(req, resp) {
+
+//  console.log("ZIP: " + req.query.zip)
+  // FIXME: Filter or control for valid values of 'zip'
+  var searchRequest = {
+    term: '',
+    location: req.query.zip || '78230',
+    categories: 'coffee',
+    radius: 8000
+  };
+
+  yelpClient.search(searchRequest).then(response => {
+     const firstResult = response.jsonBody.businesses;
+
+     const prettyJson = JSON.stringify(firstResult, null, 4);
+     console.log(prettyJson);
+
+     resp.json(firstResult);
+  });
+
 });
 
 app.listen(app.get('port'), function() {
--- js/index.js
+++ js/index.js
@@ -7,8 +7,6 @@ import Results from './results'
 import Results2 from './results2'
 import Results3 from './results3'
 import Results4 from './results4'
-import Results5 from './results5'
-import Results6 from './results6'
 
 render(
   (
@@ -19,8 +17,6 @@ render(
        <Route path="results2" component={Results2} />
        <Route path="results3" component={Results3} />
        <Route path="results4" component={Results4} />
-       <Route path="results5" component={Results5} />
-       <Route path="results6" component={Results6} />
      </Route>
    </Router>
   ),
